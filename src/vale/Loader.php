391840243ad1a70def7012353f5264c5e892bdcb
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2020-08-12 18:59:22 -0700

    Create Loader.php

---
+++ src/vale/Loader.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace vale;
+
+
+use pocketmine\plugin\PluginBase;
+use vale\api\PlayerAPI;
+use vale\api\SpecialAPI;
+use vale\commands\PhantomBag;
+use vale\sets\PhantomSet;
+
+
+class Loader extends PluginBase
+{
+
+
+    /**
+     * @var Loader
+     */
+    private static $instance;
+
+
+    /**
+     * @var \pocketmine\command\SimpleCommandMap|null
+     */
+    private $command;
+
+    public function onEnable()
+    {
+        self::$instance = $this;
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getLogger()->info("Enabling Special Sets");
+        $this->command = $this->getServer()->getCommandMap();
+        $this->getServer()->getCommandMap()->register("phantombag", new PhantomBag("phantombag", $this));
+        $this->loadSets();
+        $this->API();
+
+    }
+
+    /**
+     * @param string $set
+     */
+
+    public function loadSets()
+    {
+        new PhantomSet($this);
+
+
+    }
+
+    /**
+     * @param string $api
+     */
+
+    public function API()
+    {
+        new PlayerAPI($this);
+        new SpecialAPI($this);
+
+
+    }
+    public static function getInstance(): Loader{
+
+        return self::$instance;
+
+
+    }
+
+
+
+}
